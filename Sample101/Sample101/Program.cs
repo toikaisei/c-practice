ddc7d2d8ebc4a9d45eb5a7604dfe635121d1c006
toi <toi@freemake.co.jp>
2024-07-08 17:28:23 +0900

    c#5-10

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,43 +10,28 @@ namespace Sample103
     {
         static void Main(string[] args)
         {
-            //長さ10のint型の配列を作成する
-            int[] array = new int[10];
-            //0の個数を保持するための変数
-            int zero = 0;
+            //長さ7のint型の配列を作成する
+            int[] array = new int[7];
             //乱数を作る為のrandオブジェクト生成
             Random rand = new Random();
 
             //配列に格納した値を全て出力させるループ
             for (int i = 0; i < array.Length; i++) {
-                //array0から100までの乱数を代入する。
-                array[i] = rand.Next(0, 101);
+                //array1から10までの乱数を代入する。
+                array[i] = rand.Next(1, 11);
                 Console.Write(array[i] + " ");
             }
-
-            Console.Write("\n50以上の数:");
-            for (int i = 0; i < array.Length; i++) {
-                //arrayに格納された値が50以上のみ出力
-                if (array[i] > 50)
-                {
-                    Console.Write(array[i] + " ");
-                }
-                //arrayに格納された値が0と等しければzeroをインクリメント
-                else if (array[i] == 0) {
-                    zero++;
-                }          
-            }
-            Console.Write("\n50以下の数:");
-            //50以下の値を出力するループ
-            for (int i = 0; i < array.Length; i++) {
-                //arrayに格納された値が50以下のみ出力
-                if (array[i] < 50) {
-                    Console.Write(array[i] + " ");
-                }
-            }
-            //arrayに0が格納されいる0の個数を出力
-            Console.Write("\n0の個数:" + zero);
             Console.WriteLine("");
+            //arrayに入った数字分＊を出力
+            for (int i = 0; i < array.Length; i++) {
+                for (int j = 0; j < array[i]; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+            Console.WriteLine("");
+           
 
 
         }
