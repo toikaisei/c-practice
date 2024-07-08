b53972ef6fce994975554d7326beb3b182bd98e4
toi <toi@freemake.co.jp>
2024-07-08 16:13:07 +0900

    c#5-9

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,39 +10,42 @@ namespace Sample103
     {
         static void Main(string[] args)
         {
-            //長さ5のint型の配列を作成する
-            int[] array = new int[5];
+            //長さ10のint型の配列を作成する
+            int[] array = new int[10];
             //0の個数を保持するための変数
             int zero = 0;
-            //0より大きい値を保持する変数
-            int overnum = 0;
-            //0より小さい値を保持する変数
-            int undernum = 0;
+            //乱数を作る為のrandオブジェクト生成
             Random rand = new Random();
 
+            //配列に格納した値を全て出力させるループ
             for (int i = 0; i < array.Length; i++) {
-                //arrayに-10から10までの乱数を代入する
-                array[i] = rand.Next(-10, 11);
-                //arrayに格納した値を全て出力
+                //array0から100までの乱数を代入する。
+                array[i] = rand.Next(0, 101);
                 Console.Write(array[i] + " ");
             }
+
+            Console.Write("\n50以上の数:");
             for (int i = 0; i < array.Length; i++) {
-                
-                if (array[i] > 0)
+                //arrayに格納された値が50以上のみ出力
+                if (array[i] > 50)
                 {
-                    overnum++;
+                    Console.Write(array[i] + " ");
                 }
-                else if (array[i] < 0)
-                {
-                    undernum++;
-                }
-                else {
+                //arrayに格納された値が0と等しければzeroをインクリメント
+                else if (array[i] == 0) {
                     zero++;
-                }      
+                }          
             }
-            Console.Write("\n0よりも大きい数:" + overnum + "個");
-            Console.Write("\n0よりも小さい数:" + undernum + "個");
-            Console.Write("\n0よりも小さい数:" + zero + "個");
+            Console.Write("\n50以下の数:");
+            //50以下の値を出力するループ
+            for (int i = 0; i < array.Length; i++) {
+                //arrayに格納された値が50以下のみ出力
+                if (array[i] < 50) {
+                    Console.Write(array[i] + " ");
+                }
+            }
+            //arrayに0が格納されいる0の個数を出力
+            Console.Write("\n0の個数:" + zero);
             Console.WriteLine("");
 
 
