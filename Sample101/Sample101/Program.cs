65702b2bbdb38a97741bf0acdf38c8a350a3bf01
toi <toi@freemake.co.jp>
2024-07-10 13:37:23 +0900

    c#5-11

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,29 +10,46 @@ namespace Sample103
     {
         static void Main(string[] args)
         {
-            //長さ7のint型の配列を作成する
-            int[] array = new int[7];
+            //長さ5のint型の配列を作成する
+            int[] array = new int[5];
             //乱数を作る為のrandオブジェクト生成
             Random rand = new Random();
 
             //配列に格納した値を全て出力させるループ
             for (int i = 0; i < array.Length; i++) {
-                //array1から10までの乱数を代入する。
-                array[i] = rand.Next(1, 11);
+                //arrayに、1から100までの乱数を代入する。
+                array[i] = rand.Next(1, 101);
                 Console.Write(array[i] + " ");
             }
             Console.WriteLine("");
-            //arrayに入った数字分＊を出力
+            Console.Write("\n0以上60未満:");
+            //0以上60未満の,値のみ出力
             for (int i = 0; i < array.Length; i++) {
-                for (int j = 0; j < array[i]; j++)
-                {
-                    Console.Write("*");
+                if (array[i] >= 0 && array[i] < 60) {
+                    Console.Write(array[i] + " ");
                 }
-                Console.WriteLine("");
             }
-            Console.WriteLine("");
-           
-
+            Console.Write("");
+            Console.Write("\n60以上80未満:");
+            //60以上80未満の,値のみ出力
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= 60 && array[i] < 80)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+            Console.Write("");
+            Console.Write("\n80以上:");
+            //80以上の,値のみ出力
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] >= 80)
+                {
+                    Console.WriteLine(array[i] + " ");
+                }
+            }
+            Console.Write("");
 
         }
     }
