8145569ea6271d8b4c122aebc866c315741573d4
toi <toi@freemake.co.jp>
2024-07-10 15:19:53 +0900

    c#5-12

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,46 +10,30 @@ namespace Sample103
     {
         static void Main(string[] args)
         {
-            //長さ5のint型の配列を作成する
-            int[] array = new int[5];
-            //乱数を作る為のrandオブジェクト生成
-            Random rand = new Random();
-
-            //配列に格納した値を全て出力させるループ
-            for (int i = 0; i < array.Length; i++) {
-                //arrayに、1から100までの乱数を代入する。
-                array[i] = rand.Next(1, 101);
-                Console.Write(array[i] + " ");
-            }
-            Console.WriteLine("");
-            Console.Write("\n0以上60未満:");
-            //0以上60未満の,値のみ出力
-            for (int i = 0; i < array.Length; i++) {
-                if (array[i] >= 0 && array[i] < 60) {
-                    Console.Write(array[i] + " ");
-                }
-            }
-            Console.Write("");
-            Console.Write("\n60以上80未満:");
-            //60以上80未満の,値のみ出力
-            for (int i = 0; i < array.Length; i++)
+            //3×3の二次元配列を作成
+            int[,] array = new int[3,3];
+            //最大値を保持する変数
+            int overnum;
+            //最小値を保持する変数
+            int undernum;
+            int m, n;
+            Random rnd = new Random();
+            //  二次元配列に値を代入
+            for (m = 0; m < array.GetLength(0); m++)
             {
-                if (array[i] >= 60 && array[i] < 80)
+                for (n = 0; n < array.GetLength(1); n++)
                 {
-                    Console.Write(array[i] + " ");
+                    array[m, n] = rnd.Next(1, 11);
+                    Console.Write(array[m, n] + " ");
                 }
+                Console.WriteLine("");                
             }
-            Console.Write("");
-            Console.Write("\n80以上:");
-            //80以上の,値のみ出力
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] >= 80)
-                {
-                    Console.WriteLine(array[i] + " ");
-                }
-            }
-            Console.Write("");
+            //arrayの最大値を取得
+            overnum = array.Cast<int>().Max();
+            //arrayの最小値を取得
+            undernum = array.Cast<int>().Min();
+            Console.Write("\n最大値:" + overnum);
+            Console.WriteLine("\n最小値:" + undernum);
 
         }
     }
