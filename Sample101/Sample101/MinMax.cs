b5616274b91f69822ec460f54c32ed1c47cfdf3f
toi <toi@freemake.co.jp>
2024-07-10 17:12:00 +0900

    C#6-1

---
+++ Sample101/Sample101/MinMax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    class MinMax
+    {
+        public int Max(int n1, int n2, int n3)
+        {
+            if (n1 > n2 && n1 > n3)
+            {
+                return n1;
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                return n2;
+            }
+            else
+            {
+                return n3;
+            }
+           
+        }
+        //  最小値の取得
+        public int Min(int n1, int n2, int n3)
+        {
+            if (n1 < n2 && n1 < n3)
+            {
+                return n1;
+            }
+            else if (n2 < n1 && n2 < n3)
+            {
+                return n2;
+            }
+            else
+            {
+                return n3;
+            }
+        }
+    }
+}
--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -4,37 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sample103
+namespace Sample601
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //3×3の二次元配列を作成
-            int[,] array = new int[3,3];
-            //最大値を保持する変数
-            int overnum;
-            //最小値を保持する変数
-            int undernum;
-            int m, n;
-            Random rnd = new Random();
-            //  二次元配列に値を代入
-            for (m = 0; m < array.GetLength(0); m++)
-            {
-                for (n = 0; n < array.GetLength(1); n++)
-                {
-                    array[m, n] = rnd.Next(1, 11);
-                    Console.Write(array[m, n] + " ");
-                }
-                Console.WriteLine("");                
-            }
-            //arrayの最大値を取得
-            overnum = array.Cast<int>().Max();
-            //arrayの最小値を取得
-            undernum = array.Cast<int>().Min();
-            Console.Write("\n最大値:" + overnum);
-            Console.WriteLine("\n最小値:" + undernum);
-
+            MinMax m = new MinMax();
+            int a = 4, b = 2, c = 6;
+            Console.WriteLine("{0}と{1}と{2}のうち、最大のものは{3}", a, b, c, m.Max(a, b, c));
+            Console.WriteLine("{0}と{1}と{2}のうち、最小のものは{3}", a, b, c, m.Min(a, b, c));
         }
     }
 }
