cb7bd6c7f0cbac065526768898d1bc154332d8f8
toi <toi@freemake.co.jp>
2024-07-12 13:52:27 +0900

    c#6-2

---
+++ Sample101/Sample101/Calc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    class Calc
+    {
+        public double Add(double a, double b) {
+            return a + b;
+        }
+        public double Sub(double c, double d)
+        {
+            return c - d;
+        }
+        public double Mul(double e, double f)
+        {
+            return e * f;
+        }
+        public double Div(double g, double h)
+        {
+            return g / h;
+        }
+    }
+}
--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,10 +10,12 @@ namespace Sample601
     {
         static void Main(string[] args)
         {
-            MinMax m = new MinMax();
-            int a = 4, b = 2, c = 6;
-            Console.WriteLine("{0}と{1}と{2}のうち、最大のものは{3}", a, b, c, m.Max(a, b, c));
-            Console.WriteLine("{0}と{1}と{2}のうち、最小のものは{3}", a, b, c, m.Min(a, b, c));
+            Calc c = new Calc();
+            double a = 4.1, b = 2.3;
+            Console.WriteLine(a + " + " + b + " = " + c.Add(a, b));
+            Console.WriteLine(a + " - " + b + " = " + c.Sub(a, b));
+            Console.WriteLine(a + " * " + b + " = " + c.Mul(a, b));
+            Console.WriteLine(a + " / " + b + " = " + c.Div(a, b));
         }
     }
 }
