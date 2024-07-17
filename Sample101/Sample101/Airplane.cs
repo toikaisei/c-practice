ca78cbdabacb3c9afafe83fd7b9f03e90b4c9187
toi <toi@freemake.co.jp>
2024-07-17 15:49:00 +0900

    c#応用3-1継承

---
+++ Sample101/Sample101/Airplane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample601
+{
+    class Airplane
+    {
+        public void Fly()
+        {
+            Console.WriteLine("飛行する");
+        }
+    }
+}
--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,19 +10,14 @@ namespace Sample601
     {
         static void Main(string[] args)
         {
-            Counter c1, c2;
-            c1 = new Counter();
-            c2 = new Counter();
-            c1.Increment();
-            c2.Increment();
-            c2.Increment();
-            c2.Reset();
-            c1.Increment();
-            c1.Increment();
-            c2.Increment();
-            Console.WriteLine("c1のカウント数：" + c1.Count);
-            Console.WriteLine("c2のカウント数：" + c2.Count);
-            Console.WriteLine("トータルのカウント数:{0}", Counter.totalCount);
+            Fighter f = new Fighter();      //  戦闘機クラス
+            Airplane a = new Airplane();    //  飛行機クラス
+            //  飛行機が飛行する
+            a.Fly();
+            //  戦闘機が飛行する
+            f.Fly();
+            //  戦闘機が戦闘する
+            f.Fight();
         }
     }
 }
