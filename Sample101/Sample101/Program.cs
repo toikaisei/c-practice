46d8cd33715a8cd83b53dd2ff6192f3499e33a2a
toi <toi@freemake.co.jp>
2024-07-22 16:52:44 +0900

    c#応用6-1List

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -10,24 +10,54 @@ namespace Sample601
     {
         static void Main(string[] args)
         {
-            AlarmClock ac = new AlarmClock();   //  アラーム付き時計クラスのインスタンスを生成
-            IAlarm ar = (IAlarm)ac;
-            IClock cl = (IClock)ac;
-            FuncAlarm(ar);
-            FuncClock(cl);
+            int randomValue;
+            int result;
+            List<int> evenNumbers = new List<int>();
+            List<int> oddNumbers = new List<int>();
+            Random r = new Random();
+
+            //乱数で出た値が、0になるまで繰り返し出力するループ
+            while (true) {
+                //0～10の乱数生成
+                randomValue = r.Next(0, 11);
+                result = randomValue % 2;
+                //出力された整数を(偶数)をListに格納
+                if (randomValue == 0)
+                {
+                    Console.WriteLine("0で終了" + randomValue);
+                    break;
+                }
+                //出力された整数を(奇数)をListに格納
+                else if (result != 0)
+                {
+                    oddNumbers.Add(randomValue);
+                    Console.WriteLine("奇数0～10の値を出力:" + randomValue);
+                }
+                //0が出たら終了
+                else if (result == 0)
+                {
+                    evenNumbers.Add(randomValue);
+                    Console.WriteLine("偶数0～10の値を出力:" + randomValue);
+                }
+            }
+            Console.WriteLine();
+            //Listに格納した偶数を出力
+            Console.Write("偶数:");
+            foreach (int i in evenNumbers) {
+                Console.Write(i + "" + ",");
+            }
+            Console.WriteLine();
+
+            //Listに格納した奇数を出力
+            Console.Write("奇数:");
+            foreach (int i in oddNumbers)
+            {
+                Console.Write(i + "" + ",");
+            }
+            Console.WriteLine();
+
+
         }
-        //  アラームとしての処理
-        public static void FuncAlarm(IAlarm alarm)
-        {
-            alarm.SetAlarm();   //  アラームのセット
-            alarm.Alarm();      //  アラームを鳴らす
-            alarm.StopAlarm();  //  アラームを止める
-        }
-        //  時計としての処理
-        public static void FuncClock(IClock clock)
-        {
-            clock.AdjustTime(); //  時間を調整する
-            clock.ShowTime();   //  時間を表示する
-        }
+       
     }    
 }
