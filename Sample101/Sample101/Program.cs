2e849e48db3ff0706da29be53b44b4ebedcb82ef
toi <toi@freemake.co.jp>
2024-07-24 14:16:32 +0900

    c#応用7-1デリゲート

--- Sample101/Sample101/Program.cs
+++ Sample101/Sample101/Program.cs
@@ -8,56 +8,22 @@ namespace Sample601
 {
     class Program
     {
+        //  デリゲート
+        delegate void Delfunc();
+        static void Func1()
+        {
+            Console.WriteLine("Func1");
+        }
+        static void Func2()
+        {
+            Console.WriteLine("Func2");
+        }
         static void Main(string[] args)
         {
-            int randomValue;
-            int result;
-            List<int> evenNumbers = new List<int>();
-            List<int> oddNumbers = new List<int>();
-            Random r = new Random();
-
-            //乱数で出た値が、0になるまで繰り返し出力するループ
-            while (true) {
-                //0～10の乱数生成
-                randomValue = r.Next(0, 11);
-                result = randomValue % 2;
-                //出力された整数を(偶数)をListに格納
-                if (randomValue == 0)
-                {
-                    Console.WriteLine("0で終了" + randomValue);
-                    break;
-                }
-                //出力された整数を(奇数)をListに格納
-                else if (result != 0)
-                {
-                    oddNumbers.Add(randomValue);
-                    Console.WriteLine("奇数0～10の値を出力:" + randomValue);
-                }
-                //0が出たら終了
-                else if (result == 0)
-                {
-                    evenNumbers.Add(randomValue);
-                    Console.WriteLine("偶数0～10の値を出力:" + randomValue);
-                }
-            }
-            Console.WriteLine();
-            //Listに格納した偶数を出力
-            Console.Write("偶数:");
-            foreach (int i in evenNumbers) {
-                Console.Write(i + "" + ",");
-            }
-            Console.WriteLine();
-
-            //Listに格納した奇数を出力
-            Console.Write("奇数:");
-            foreach (int i in oddNumbers)
-            {
-                Console.Write(i + "" + ",");
-            }
-            Console.WriteLine();
-
-
+            Delfunc df = new Delfunc(Func1);
+            Delfunc df2 = new Delfunc(Func2);
+            df2();
         }
-       
+
     }    
 }
